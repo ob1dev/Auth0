0adf535fc938ecec9aad06590d4cdf31d55a5a2e
Oleg Burov <oleg.burov@outlook.com>
2018-04-25 18:39:06 -0700

    Configure custom scopes.

---
+++ src/OneGit.Api/Authorization/HasScopeRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneGit.Api.Authorization
+{
+  public class HasScopeRequirement : IAuthorizationRequirement
+  {
+    public string Issuer { get; }
+    public string Scope { get; }
+
+    public HasScopeRequirement(string scope, string issuer)
+    {
+      Scope = scope ?? throw new ArgumentNullException(nameof(scope), $"The parameter '{nameof(scope)}' cannot be null.");
+      Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer), $"The parameter '{nameof(issuer)}' cannot be null.");
+    }
+  }
+}
--- src/OneGit.Api/Startup.cs
+++ src/OneGit.Api/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OneGit.Api.Authorization;
 
 namespace OneGit.Api
 {
@@ -32,6 +34,17 @@ namespace OneGit.Api
         options.Authority = domain;
         options.Audience = Configuration["Auth0:ApiIdentifier"];
       });
+
+      services.AddAuthorization(options =>
+      {
+        options.AddPolicy("read:repositories", policy => policy.Requirements.Add(new HasScopeRequirement("read:repositories", domain)));
+        options.AddPolicy("create:repositories", policy => policy.Requirements.Add(new HasScopeRequirement("create:repositories", domain)));
+        options.AddPolicy("update:repositories", policy => policy.Requirements.Add(new HasScopeRequirement("update:repositories", domain)));
+        options.AddPolicy("delete:repositories", policy => policy.Requirements.Add(new HasScopeRequirement("delete:repositories", domain)));
+      });
+
+      // register the scope authorization handler
+      services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
