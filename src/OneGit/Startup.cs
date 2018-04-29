27605d397792c390f4390802811118703541620d
Oleg Burov <oleg.burov@outlook.com>
2018-04-28 23:01:51 -0700

    Update to ASP.NET Core 2.1 Preview-2.

--- src/OneGit/Startup.cs
+++ src/OneGit/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -24,6 +25,13 @@ namespace OneGit
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
       // Add authentication services
       services.AddAuthentication(options =>
       {
@@ -96,7 +104,7 @@ namespace OneGit
       });
 
       // Add framework services.
-      services.AddMvc();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
