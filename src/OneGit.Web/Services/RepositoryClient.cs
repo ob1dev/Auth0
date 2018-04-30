4922d3c9b527e7eafcd20bf703187c362c9c9ea0
Oleg Burov <oleg.burov@outlook.com>
2018-04-29 19:35:27 -0700

    Implement a simple web client for Repository WebAPI.

---
+++ src/OneGit.Web/Services/RepositoryClient.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace OneGit.Web.Services
+{
+  public class RepositoryClient
+  {
+    private HttpClient client;
+    private ILogger<RepositoryClient> logger;
+    private IHttpContextAccessor httpContextAccessor;
+
+    public RepositoryClient(HttpClient client, ILogger<RepositoryClient> logger, IHttpContextAccessor httpContextAccessor)
+    {
+      this.client = client;
+      this.logger = logger;
+      this.httpContextAccessor = httpContextAccessor;
+
+      var context = this.httpContextAccessor.HttpContext;
+      var token = context.GetTokenAsync("access_token").Result;
+
+      if (token != null)
+      {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+      }
+    }
+
+    public async Task<IEnumerable<RepositoryModel>> GetAllRepositoriesAsync()
+    {
+      try
+      {
+        var response = await client.GetAsync("api/repositories");
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsAsync<IEnumerable<RepositoryModel>>();
+      }
+      catch (HttpRequestException ex)
+      {
+        logger.LogError($"An error occured connecting to values API {ex.ToString()}");
+        return Enumerable.Empty<RepositoryModel>();
+      }
+    }
+
+    public async Task<RepositoryModel> GetRepository(Guid id)
+    {
+      try
+      {
+        var response = await client.GetAsync($"api/repositories/{id}");
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsAsync<RepositoryModel>();
+      }
+      catch (HttpRequestException ex)
+      {
+        logger.LogError($"An error occured connecting to values API {ex.ToString()}");
+        return null;
+      }
+    }
+
+    public async Task CreateNewRepository(RepositoryModel repository)
+    {
+      try
+      {
+        var response = await client.PostAsJsonAsync("api/repositories", repository);
+        response.EnsureSuccessStatusCode();
+
+        return;
+      }
+      catch (HttpRequestException ex)
+      {
+        logger.LogError($"An error occured connecting to values API {ex.ToString()}");
+        return;
+      }
+    }
+
+    public async Task UpdateRepository(RepositoryModel repository)
+    {
+      try
+      {
+        var response = await client.PutAsJsonAsync($"api/repositories/{repository.Id}", repository);
+        response.EnsureSuccessStatusCode();
+
+        return;
+      }
+      catch (HttpRequestException ex)
+      {
+        logger.LogError($"An error occured connecting to values API {ex.ToString()}");
+        return;
+      }
+    }
+
+    public async Task DeleteRepository(Guid id)
+    {
+      try
+      {
+        var response = await client.DeleteAsync($"api/repositories/{id}");
+        response.EnsureSuccessStatusCode();
+
+        return;
+      }
+      catch (HttpRequestException ex)
+      {
+        logger.LogError($"An error occured connecting to values API {ex.ToString()}");
+        return;
+      }
+    }
+  }
+}
